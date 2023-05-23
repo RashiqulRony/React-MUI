e070a70098cbc6d364a58d8b3a6b19387c3d11f0
Md Rashiqul Islam <rashiqul.islam@smeinformatics.com>
2023-05-23 18:09:46 +0600

    React MUI layout

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import Typography from "@mui/material/Typography";
+
+function Home() {
+    return(
+        <Typography paragraph>
+           Home Page
+        </Typography>
+    );
+}
+export default Home;
