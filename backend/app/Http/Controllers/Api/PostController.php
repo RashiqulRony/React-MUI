278337f01e83c3de887ff4911bcc2bb116b1aa9c
Md Rashiqul Islam <rashiqul.islam@smeinformatics.com>
2023-05-28 18:21:48 +0600

    Laravel CRUD working

---
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        try {
+            $data = Post::orderBy('id', 'desc')->get();
+            return response()->json([
+                'status' => true,
+                'data' => $data,
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage()
+            ]);
+        }
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'title' => 'required|max:255',
+            'description' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json(['status' => false, 'errors'=> $validator->messages()]);
+        }
+
+        $image = null;
+
+        try {
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $name = time() . '_Post_' . $file->getClientOriginalName();
+                $file->move('image/', $name);
+                $image = $name;
+            }
+
+            Post::create([
+                'title'       => $request->title,
+                'description' => $request->description,
+                'status'      => $request->status,
+                'image'       => $image
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Post added successfully.',
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        try {
+            $post = Post::find($id);
+            if (!empty($post)) {
+                return response()->json([
+                    'status' => true,
+                    'data' => $post,
+                    'message' => 'Post update successfully.',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Something went wrong. This Post can not delete please try again.',
+                ]);
+            }
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage(),
+            ]);
+        }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'title' => 'required|max:191',
+            'price' => 'required|max:8',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json(['status' => false, 'errors'=> $validator->messages()]);
+        }
+
+        try {
+            $post = Post::find($id);
+            if ($request->hasFile('image')) {
+                if ($post->image){
+                    unlink(public_path('/image/').$post->image);
+                }
+                $file = $request->file('image');
+                $name = time() . '_Post_' . $file->getClientOriginalName();
+                $file->move('image/', $name);
+                $image = $name;
+            }
+
+            if (!empty($post)) {
+                $post->update([
+                    'title'       => $request->title,
+                    'price'       => $request->price,
+                    'description' => $request->description,
+                    'image'       => isset($image) ? $image : $post->image,
+                    'updated_at'  => now()
+                ]);
+
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Post update successfully.',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Something went wrong. This Post can not updated please try again.',
+                ]);
+            }
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        try {
+            $post = Post::find($id);
+            if (!empty($post)) {
+                if ($post->image){
+                    unlink(public_path('/image/').$post->image);
+                }
+                $post->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Post update successfully.',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Something went wrong. This Post can not delete please try again.',
+                ]);
+            }
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage(),
+            ]);
+        }
+    }
+}
---
+++ src/pages/Posts.js
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Title from './Title';
+import {Button, Grid, Paper} from "@mui/material";
+import http from "../http";
+import {useEffect, useState} from "react";
+
+
+// Generate Order Data
+
+function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    const getPosts = () => {
+        http.get('/posts').then((response) => response.data)
+            .then((response) => {
+                setPosts(response.data)
+            }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    return (
+        <React.Fragment>
+            <div className={'row mb-3'}>
+                <div className={'col-6'}>
+                    <Title>Posts</Title>
+                </div>
+                <div className={'col-6'} style={{textAlign: "right"}}>
+                    <Button  variant="contained">Add New</Button>
+                </div>
+            </div>
+
+            <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                    <div className={'table-responsive'}>
+                        <Table size={'small'}>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>#ID</TableCell>
+                                    <TableCell>Title</TableCell>
+                                    <TableCell>Description</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {posts.map((post, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{post.id}</TableCell>
+                                    <TableCell>{post.title}</TableCell>
+                                    <TableCell>{post.description}</TableCell>
+                                </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                </Paper>
+            </Grid>
+
+        </React.Fragment>
+    );
+}
+
+export default Posts;
